a422dd75e6602588463b091da3dd52fc062ce2f5
marcsgil <marcosgildeoliveira@gmail.com>
2023-02-06 18:01:10 -0300

    worked a little bit

--- test.jl
+++ test.jl
@@ -1,7 +1,8 @@
 using FreeParaxialPropagation
 using KerrPropagation
+using InteractiveBeamVizualization
 ##
-using ThreadsX,Integrals, LinearAlgebra
+using ThreadsX, Integrals, LinearAlgebra
 kerrCube(x) = abs2(x)*x
 
 function approximatePertubation(x,y,z,l;k=1,χ=1)
@@ -17,19 +18,16 @@ end
 rs = LinRange(-5,5,1024)
 zs = LinRange(0,.5,32)
 
-χ = 1e-6
+χ = 0
 l = 2
 ##
-ψ₀ = LG(rs,rs,0,l=l) |> cu
+ψs = LG(rs,rs,0,l=l) |> cu
 
-ψs = convert(Array{ComplexF64},kerrPropagation(ψ₀,rs,rs,zs,128, χ = χ))
+ψs = convert(Array{ComplexF64},kerrPropagation(ψs[:,:,1],rs,rs,zs,128, χ = 1))
+ψs[:,:,1]
 δψs = ψs - LG(rs,rs,zs,l=l)
+interactive_vizualization(δψs, rs, rs, zs)
 ##
 approx_ψs = ThreadsX.map(r->approximatePertubation(r[1],r[2],last(zs),l,χ = χ),Iterators.product(rs,rs))
 
-
 overlap(view(ψs,:,:,length(zs)) - view(ψ0s,:,:,length(zs)), approx_ψs)
-##
-using InteractiveBeamVizualization
-interactive_vizualization(δψs, rs, rs, zs)
-##
