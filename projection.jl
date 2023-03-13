56ccb78e58c8ab4c45f637ae3c89d8c90d8d7c0e
marcsgil <marcosgildeoliveira@gmail.com>
2023-03-13 16:44:28 -0300

    update

--- projection.jl
+++ projection.jl
@@ -8,18 +8,23 @@ function overlap(ψ₁::AbstractArray{T,3},ψ₂::AbstractArray{T,3},area_elemen
     [ overlap(view(ψ₁,:,:,n),view(ψ₂,:,:,n),area_element) for n in axes(ψ₁,3) ]
 end
 ##
-rs = LinRange(-6,6,1024)
-zs = LinRange(0,.5,32)
+rs = LinRange(-12,12,2048)
+zs = LinRange(0,1,32)
 
-χ = 10^2
-l₀ = 1
+χ = 1
+l₀ = 4
 ##
 ψ₀ = LG(rs,rs,0,l=l₀) |> cu
-ψs = convert(Array{ComplexF64},kerr_propagation(ψ₀,rs,rs,zs,1024,χ=χ))
+ψs = convert(Array{ComplexF64},kerr_propagation(ψ₀,rs,rs,zs,2048,χ=χ))
 ψ0s = convert(Array{ComplexF64},free_propagation(ψ₀,rs,rs,zs))
 δψs = ψs - ψ0s
 ##
 using Plots
+
+default()
+default(width=3,size=(600,400), markersize = 5, tickfontsize=12, 
+labelfontsize=12,fontfamily="Computer Modern",dpi=1000,legendfontsize=10,)
+
 os = reduce(hcat,[overlap(LG(rs,rs,zs,p=p,l=l₀,γ₀ = 1/√3),δψs,(rs[2]-rs[1])^2) for p in 0:abs(l₀)+2])
 
 plot(zs,os)
