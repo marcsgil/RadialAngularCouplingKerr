cd4d6fb7a8de39e1137f78f871a8978961667f0a
Marcos Gil <marcosgildeoliveira@gmail.com>
2023-03-21 12:29:34 -0300

    added plots

--- projection.jl
+++ projection.jl
@@ -44,16 +44,16 @@ function C2(z,p,l)
     sum( c(q,l) * Φ(z,r) * Λ(3,r,p,l) * Λ(3,r,q,l) for q in 0:abs(l), r in 0:10^3 ) * im / 4
 end
 ##
-χ = 20
+χ = 1000
 l₀ = 2
-Z = .3
+Z = 1
 ##
 rs = LinRange(-3,3,512)
 zs = LinRange(0,Z,64)
 zs_scatter = LinRange(0,Z,length(zs)÷4)
 
 ψ₀ = lg(rs,rs,0,l=l₀) |> CuArray
-ψs = kerr_propagation(ψ₀,rs,rs,zs,1024,χ=χ,k=2)
+ψs = kerr_propagation(ψ₀,rs,rs,zs,2048,χ=χ,k=2)
 FreeParaxialPropagation.animate(ψs)
 
 
@@ -63,14 +63,13 @@ FreeParaxialPropagation.animate(ψs)
 Cs = stack(overlap(lg(rs,rs,zs,p=p,l=l₀,w0 = 1/√3,k=2)|> CuArray ,δψs,(rs[2]-rs[1])^2) for p in 0:abs(l₀)+2)
 
 C1s = [abs(C1(z,p,l₀)) for z in zs_scatter, p in 0:abs(l₀)]
-jldsave("Data/l=$(l₀)_g=$(χ)_z=$Z.jld2";ψs,Cs,C1s,zs,zs_scatter,χ,l₀)
-#C2s = [abs(C2(z,p,l₀)) for z in zs_scatter, p in 0:abs(l₀)+2]
+C2s = [abs(C2(z,p,l₀)) for z in zs_scatter, p in 0:abs(l₀)+2]
 ##
 p = plot(zs,Cs,
-    xlabel=L"Z",
-    ylabel=L"100 \times |C_{p%$l₀} \ |/g ",
-    xformatter = x->x,
-    yformatter = y->100*y,
+    xlabel=L"10 \times Z",
+    ylabel=L"1000 \times |C_{p%$l₀} \ |/g ",
+    xformatter = x->10*x,
+    yformatter = y->1000*y,
     label=[L"p = %$p" for _ in 1:1, p in 0:size(Cs,2)-1],
     annotations = ((.15,.85), Plots.text(L"g=%$χ",15)),
     legend = :outerright)
@@ -94,18 +93,22 @@ q = plot(zs,modified_Cs,
 #png(q,"MPlots/l=$(l₀)_g=$(χ)_z=$(last(zs))")
 
 
-jldsave("Data/l=$(l₀)_g=$(χ)_z=$Z.jld2";ψs,Cs,C1s,zs,zs_scatter,χ,l₀)
+jldsave("Data/l=$(l₀)_g=$(χ)_z=$Z.jld2";ψs,Cs,C1s,C2s,zs,zs_scatter,χ,l₀)
 save("Gifs/l=$(l₀)_g=$(χ)_z=$Z.gif",FreeParaxialPropagation.animate(ψs,ratio=.5))
 ##
 
-function make_plot(zs,zs_scatter,Cs,Ds,g,show_tick_legend)
+function make_plot(zs,zs_scatter,Cs,Ds,g,show_tick_legend,isfirst)
     p = plot(zs,Cs,
     ylabel=L"100 \times |C_{p%$l₀} \ |/g ",
     yformatter = y->100*y,
-    annotations = ((.12,.85), Plots.text(L"g=%$g",12)),size=(354,200),left_margin=4Plots.mm)
+    annotations = ((.15,.85), Plots.text(L"g=%$g",12)),size=(354,200),left_margin=4Plots.mm)
+
+    if isfirst
+        plot!(p,title="(b)")
+    end
 
     if show_tick_legend
-        plot!(xlabel=L"Z",bottom_margin=-3Plots.mm)
+        plot!(xlabel=L"Z",xformatter = x->x,bottom_margin=-3Plots.mm)
     else
         plot!(p,xformatter=_->"",bottom_margin=-8.1Plots.mm)
     end
@@ -115,11 +118,13 @@ end
 
 ps = []
 
-for g ∈ (.1,1,20)
+for g ∈ (1,10,80)
     _load(name) = load("Data/l=2_g=$(g)_z=8.jld2",name)
     push!(ps,make_plot(_load("zs"),_load("zs_scatter"),
-    _load("Cs"),_load("C2s"),g,length(ps)==2))
+    _load("Cs"),_load("C2s"),g,length(ps)==2,length(ps)==0))
 end
 
-p = plot(ps[1],ps[2],ps[3],layout=(3,1),size=(400,800))
-#png("Plots/z=1.png")
+p2 = plot(ps[1],ps[2],ps[3],layout=(3,1),size=(400,800))
+#png("Plots/big_plot.png")
+
+plot(p1,p2,layout=(1,2),size=(800,800))
